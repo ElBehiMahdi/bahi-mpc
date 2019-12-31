40536228e4cbcc7f177e309496d2ad924d9079f7
mahdi.bahi <mahdi.bahi@gmail.com>
2019-12-31 17:11:07 +0100

    first commit

---
+++ app/src/main/java/com/elbehi/mpc/InstantMessage.java
@@ -0,0 +1,27 @@
+package com.elbehi.mpc;
+
+
+
+class InstantMessage {
+
+    private String message;
+    private String author;
+
+    InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
